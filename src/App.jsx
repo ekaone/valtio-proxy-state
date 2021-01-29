a829ef7a2283dc260c859d4a5bf84edb34587025
ekaone <ekaone3033@gmail.com>
2021-01-29 18:51:53 +0700

    add proxy function

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,21 @@ const state = proxy({
   useDecrement: function () {
     --this.count;
   },
+  seats: [
+    { id: "A1", number: "A1", occupied: true },
+    { id: "A2", number: "A2", occupied: true },
+    { id: "A3", number: "A3", occupied: false },
+    { id: "A4", number: "A4", occupied: false },
+    { id: "A5", number: "A5", occupied: true },
+    { id: "A6", number: "A6", occupied: true },
+    { id: "A7", number: "A7", occupied: false },
+  ],
+  selected: [],
+  useSelected: function (id) {
+    const order = this.seats.find((seat) => seat.id === id);
+    state.selected.push({ ...order });
+    console.log(state.selected);
+  },
 });
 
 function App() {
@@ -24,6 +39,18 @@ function App() {
         {snapshot.text}
         <button onClick={() => state.useIncrement()}>Increment</button>{" "}
         <button onClick={() => state.useDecrement()}>Decrement</button>
+        <br />
+        Selected: {snapshot.selected.length}
+        <br />
+        {JSON.stringify(snapshot.selected, null, 2)}
+        <br />
+        <ul>
+          {snapshot.seats.map((seat) => (
+            <li key={seat.id} onClick={() => snapshot.useSelected(seat.id)}>
+              {seat.number} {seat.occupied === true ? "Isi" : "Kosong"}
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
